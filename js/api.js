63e16d42b4d6e288020f4c096adb52630e4a193c
deepnode13 <deepnodeagency@gmail.com>
2026-08-17 11:07:01 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -86,14 +86,6 @@ async function apiGetDashboard(
 
   return response.data;
 }
-
-const dashboard =
-  await apiGetDashboard(
-    8,
-    2026,
-    '2026-08-18'
-  );
-
 /**
  * Obtiene hábitos.
  */
