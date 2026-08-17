dddc23702676730458b58b2737de1c45d1688e62
deepnode13 <deepnodeagency@gmail.com>
2026-08-17 04:11:45 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,7 @@
+/**
+ * Configuración pública del frontend.
+ */
 
+const APP_CONFIG = {
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzqDpAhZIiB9KhdNdS10wK5B1hpB38wQ4P7n67qImWS2ldXUUzEdP4jW5126SC5yPgG/exec'
+};
