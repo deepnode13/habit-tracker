14981835531a11a77de2b45b2b0786a84fafbc1e
deepnode13 <deepnodeagency@gmail.com>
2026-08-17 11:09:37 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ async function iniciarApp() {
     if (
       !APP_CONFIG.API_BASE_URL ||
       APP_CONFIG.API_BASE_URL.includes(
-        'https://script.google.com/macros/s/AKfycbzqDpAhZIiB9KhdNdS10wK5B1hpB38wQ4P7n67qImWS2ldXUUzEdP4jW5126SC5yPgG/exec'
+        'PEGA_AQUI'
       )
     ) {
       throw new Error(
