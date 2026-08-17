f78f1db8db260f5206b41046212619837e43dfd6
deepnode13 <deepnodeagency@gmail.com>
2026-08-17 04:41:10 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,60 @@
+/**
+ * Capa de comunicación con Habit Tracker API.
+ */
 
+/**
+ * Realiza una solicitud GET a Apps Script.
+ *
+ * @param {string} action
+ * @param {Object} params
+ * @return {Promise<Object>}
+ */
+async function apiGet(action, params = {}) {
+
+  const url =
+    new URL(
+      APP_CONFIG.API_BASE_URL
+    );
+
+  url.searchParams.set(
+    'action',
+    action
+  );
+
+  Object.entries(params)
+    .forEach(([key, value]) => {
+
+      if (
+        value !== undefined &&
+        value !== null &&
+        value !== ''
+      ) {
+        url.searchParams.set(
+          key,
+          value
+        );
+      }
+
+    });
+
+  const response =
+    await fetch(url.toString());
+
+  if (!response.ok) {
+    throw new Error(
+      `Error HTTP: ${response.status}`
+    );
+  }
+
+  const data =
+    await response.json();
+
+  if (!data.success) {
+    throw new Error(
+      data.message ||
+      'La API devolvió un error'
+    );
+  }
+
+  return data;
+}
